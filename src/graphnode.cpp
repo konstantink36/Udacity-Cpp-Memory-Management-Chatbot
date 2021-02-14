f48558637328a1a0b541dce3c70a8aae0ac8d827
konstantink36 <76598075+konstantink36@users.noreply.github.com>
2021-02-14 16:37:10 +0100

    Update graphnode.cpp

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -26,8 +26,6 @@ void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
     _childEdges.push_back(std::move(edge));
 }
 
-//// STUDENT CODE
-////
 void GraphNode::MoveChatbotHere(ChatBot chatbot)
 {
     _chatBot = std::move(chatbot);
@@ -38,16 +36,12 @@ void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
 {
     newNode->MoveChatbotHere(std::move(_chatBot));
 }
-////
-//// EOF STUDENT CODE
+
 
 GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
 {
-    //// STUDENT CODE
-    ////
+
 
  return _childEdges[index].get();
 
-    ////
-    //// EOF STUDENT CODE
 }
